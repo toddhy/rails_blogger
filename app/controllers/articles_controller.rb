5b08cd90cfd38dd4fc47d100191f13939dc8de3b
yoshtodd <toddyosh@gmail.com>
2018-08-06 18:17:04 +0000

    Create and view new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,23 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new
+		@article.title = params[:article][:title]
+		@article.body = params[:article][:body]
+		@article.save
+		redirect_to article_path(@article)
 	end
 end
 
