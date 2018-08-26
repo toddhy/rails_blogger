63a948ca68d6ab2ea76f1f9b2f0518e92a1d53d4
Todd <toddhy@hawaii.edu>
2018-08-26 10:58:00 -1000

    Add flash messages and set root.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,18 +17,23 @@ class ArticlesController < ApplicationController
 		@article.title = params[:article][:title]
 		@article.body = params[:article][:body]
 		@article.save
+		flash.notice = "Article '#{@article.title}' Created."
 		redirect_to article_path(@article)
 	end
 
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
+		flash.notice = "Article '#{@article.title}' Deleted."
 		redirect_to articles_path
 	end
 
 	def update
 		@article = Article.find(params[:id])
 		@article.update(article_params)
+		
+		flash.notice = "Article '#{@article.title}' Updated!"
+
 		redirect_to article_path(@article)
 	end
 	
